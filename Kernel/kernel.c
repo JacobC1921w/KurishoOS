957126092b876bf5bf951ecba5535c4bc444f40a
JacobC1921w <j@c0b.dev>
2025-11-13 14:33:24 +1100

    Added some nitpicky stuff and updated TODO.md, ignore this commit.

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -9,12 +9,15 @@ void main() {
     print_string(BUILD_NUMBER);
     print_string("\n-------------------------------\n");
 
-    print_string("Installing interrupt service routines (ISRs).\n");
+    print_string("Installing interrupt service routines (ISRs)...");
     isr_install();
+    print_string(" DONE!\n");
 
-    print_string("Enabling external interrupts.\n");
+    print_string("Enabling external interrupts...");
     asm volatile("sti");
+    print_string(" DONE!\n");
 
-    print_string("Initializing keyboard (IRQ 1).\n");
+    print_string("Initializing keyboard (IRQ 1)...");
     initialize_keyboard();
+    print_string(" DONE!\n");
 }
--- TODO.md
+++ TODO.md
@@ -6,3 +6,7 @@
 4. Make the Makfile more pretty, and allow for more dynamic compiling, rather than specifying the individual files (i.e. use wildcards)
 5. Fix keyboard driver unknown key up for: insert, delete, arrows * 4, page(up/down), left control, left alt, windows key being left ctrl+escape(?), and right control/alt not being recognised(+ unknown key up)
 6. Make exception messages more dynamic (don't need repeated `Exception: Future reservation` and `Exception: External interrupt (0x--)`)
+7. Create a simple working shell
+8. Create filesystem stuff
+9. Create simple networking driver
+10. Make grpahics works
